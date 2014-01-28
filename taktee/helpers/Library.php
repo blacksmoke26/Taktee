898333093d947b200ee6225f7033ed1cbfa8e65f
Junaid Atari <mj.atari@gmail.com>
2014-01-28 18:38:24 +0500

    new methods added, v0.2

--- taktee/helpers/Library.php
+++ taktee/helpers/Library.php
@@ -12,11 +12,51 @@
  * @package		Taktee
  * @author		Junaid Atari <mj.atari@gmail.com>
  * @copyright	2014 Junaid Atari
- * @version		0.1
+ * @version		0.2
  * @license		http://www.apache.org/licenses/LICENSE-2.0.html
  * @see			TakteeFunction
  */
 class TakteeLibrary
 {
-
+	protected $options = array ();
+	
+	/**
+	 * Constructor method
+	 * Note: Please do not add addtional paramaters or optional
+	 *
+	 * @access public
+	 * @author Junaid Atari <mj.atari@gmail.com>
+	 *
+	 * @return void
+	 */
+	public function __construct ( $options = array () )
+	{
+		$this->options = $options;
+	}
+	
+	/**
+	 * Basic information of about library
+	 *
+	 * @access public
+	 * @author Junaid Atari <mj.atari@gmail.com>
+	 *
+	 * @return string
+	 */
+	public function _baseInfo ()
+	{
+		return 'called ' . __FUNCTION__;
+	}
+	
+	/**
+	 * Help/reference about using library.
+	 *
+	 * @access public
+	 * @author Junaid Atari <mj.atari@gmail.com>
+	 *
+	 * @return string
+	 */
+	public function help ()
+	{
+		return 'called ' . __FUNCTION__;
+	}
 }
